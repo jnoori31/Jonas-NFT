191cbbd0c680aa202baaea6826c59f38b7c4448e
felix habert <uclqfbf@ucl.ac.uk>
2021-05-27 22:17:43 +0200

    added routes && controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,18 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :nfts, except: :index do
-
+    # member do
+      # get 'discover', to: "nfts#discover"
+      # get 'nfts/:id/discover', to: "nfts#discover"
+    # end
+    collection do
+      get 'discover', to: "nfts#discover"
+    end
   end
+  resources :comments, only: :create
+  get 'users/:id', to: "comments#create"
+  resources :users, only: [:show] do
+    resources :chatrooms, only: :create
+  end
+  resources :chatrooms, only: :show
 end
