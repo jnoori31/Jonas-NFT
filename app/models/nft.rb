fa5f71d13ba4be9137ae2f78afd087716c14958c
Jonas Noori <jonasgeneralassembly@gmail.com>
2021-05-29 14:51:14 +0100

    schema correct now

--- app/models/nft.rb
+++ app/models/nft.rb
@@ -1,4 +1,4 @@
 class Nft < ApplicationRecord
-  belongs_to :users
+  belongs_to :user
   has_many :comments
 end
--- db/migrate/20210527201403_create_nfts.rb
+++ db/migrate/20210527201403_create_nfts.rb
@@ -5,7 +5,7 @@ class CreateNfts < ActiveRecord::Migration[6.0]
       t.string :media_type
       t.integer :price
       t.string :category
-      t.references :users, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.text :description
 
       t.timestamps
