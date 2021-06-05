c50f0b617ab69d37621e809c83e404386d6320be
felix habert <uclqfbf@ucl.ac.uk>
2021-06-05 16:35:06 +0200

    new nft starting

---
+++ app/javascript/controllers/nftCreation_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+    static targets = ['nftToken']
+
+    searchNft() {
+        const idToken = this.nftTokenTarget.value;
+        console.log(idToken);
+        const nftUrl = `https://metadata.mintable.app/mintable_gasless/${idToken}`;
+        fetch(nftUrl)
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data);
+            })
+    }
+}
