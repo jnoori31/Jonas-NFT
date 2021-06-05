9d0f3c0bb0d1e29653c68788ce91576951b5bd20
Pierre-Simon Ntiruhungwa <psntir@gmail.com>
2021-06-05 16:39:55 +0100

    created the NFTs cards

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
-        @current_user = User.find_by(id: current_user.id)
         @chatrooms = Chatroom.where(client_id: current_user.id)
+        @Nft = Nft.all
     end
 end
