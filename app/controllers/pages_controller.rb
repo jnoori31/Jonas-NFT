db02ecd1f7536ce105f4c0228b90135302a3c9b9
Alexandre Morey <alexander006_9@hotmail.com>
2021-06-10 19:26:35 +0100

    ajaxfollow

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -33,6 +33,8 @@ class PagesController < ApplicationController
   def toggle_follows
     @user = User.find(params[:user_id])
     current_user.favorited?(@user) ? current_user.unfavorite(@user) : current_user.favorite(@user)
+    # redirect_to root_path
+    # (`.like-icon[data-id='${userId}']
   end
 end
 
