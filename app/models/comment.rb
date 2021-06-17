b46c76f2acac179e52b3c2b348d95e899ad3246f
Alexandre Morey <alexander006_9@hotmail.com>
2021-06-17 20:15:13 +0100

    notification finally done

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,14 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :nft
+  has_one :notification
   validates :content, presence: true
+
+  after_create :create_notification
+
+  private
+
+  def create_notification
+    Notification.create(user: nft.user, comment: self)
+  end
 end
