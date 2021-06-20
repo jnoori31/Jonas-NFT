5e116a602387798e08219ee010bd3692a67e1722
felix habert <uclqfbf@ucl.ac.uk>
2021-06-20 22:21:12 +0200

    small changes

--- app/javascript/components/nft_chatroom.js
+++ app/javascript/components/nft_chatroom.js
@@ -2,6 +2,7 @@ const nftChatroom = () => {
     const divBtn = document.querySelector('.nft-chatroom');
     const hiddenField = document.getElementById('hidden-field-chatroom');
     const uploadNft = document.getElementById('upload-nft-desk');
+    const picChat = document.querySelector('.picture-message');
     if (divBtn) {
         const links = divBtn.querySelectorAll('.img-over');
         links.forEach((link) => {
