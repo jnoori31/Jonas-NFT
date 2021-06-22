478127d8f288ced1d8ee6541b7586598c37f5d5c
felix habert <uclqfbf@ucl.ac.uk>
2021-06-22 13:50:03 +0200

    added some changes style

--- app/javascript/plugins/select2.js
+++ app/javascript/plugins/select2.js
@@ -2,22 +2,27 @@ import 'select2';
 import $ from 'jquery';
 
 const initSelect2 = () => {
+    // making select2 work
     $('select.select2-category').select2({
         allowClear: true,
         closeOnSelect: true
     });
+    // 
     const submitCat = document.getElementById('submit-category');
     const btnSubmit = document.getElementById('btn-submit-category');
     if (btnSubmit) {
         btnSubmit.addEventListener('click', (event) => {
             const ulCategories = document.querySelector('.select2-selection__rendered');
-            const elements = ulCategories.querySelectorAll('.select2-selection__choice__display')
+            // getting the ul which contains the li of the selected options, you can see in the inspect
+            const elements = ulCategories.querySelectorAll('.select2-selection__choice__display');
+            // selectinng all options from the ul
             if (ulCategories) {
                 let categories = "";
                 elements.forEach((el) => {
                     categories += `${el.innerText.toLowerCase()} `;
                 })
-                submitCat.value = categories;
+                // purpose its to get all categories in lower case to match the seed style
+                submitCat.value = categories; //see the home page
             }
         })
     }
